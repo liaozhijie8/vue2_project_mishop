ec505ea74525324f818e9cc4a63fbcab480bddaa
liaozhijie <249289578@qq.com>
2022-10-17 11:27:31 +0800

    init

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8001',
+        changeOrigin: true,
+        // 拼接一个路径
+        pathRewrite: {
+          '/api': '/api'
+        }
+      }
+    }
+  }
 })
